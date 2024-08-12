02931d48bbb877145058c21fb1100c196fd8ce2f
josaphath <josahath049@gmail.com>
2024-08-12 13:06:33 +0100

    installation et configuation de express js

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(" l'Url recue", req.url);
+  next(); // Passe à la route suivante
+});
+
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.send("Bonjour, Express.js!de jojo");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Routes about");
+});
+app.get("/contacter", (req, res) => {
+  res.send("Conctacter nous");
+});
+
+app.get("/user/:name", (req, res) => {
+  const name = req.params.name;
+  res.send(`Bonjour, ${name}!`);
+});
+
+app.post("/data", (req, res) => {
+  res.send("Données envoyées avec POST");
+});
+
+app.put("/data", (req, res) => {
+  res.send("Données mises à jour avec PUT");
+});
+
+app.delete("/data", (req, res) => {
+  res.send("Données supprimées avec DELETE");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+});
